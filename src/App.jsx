f2f9b692f3502f3e4721cdc2863ae22d4a7a92db
David <davidgonzalezj97@gmail.com>
2024-01-02 19:20:31 +0100

    Exercise 5.1 Making user login functionality to see the blogs

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,80 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
+    async function getBlogs(){
+      const blogs = await blogService.getAll()
       setBlogs( blogs )
-    )  
+    }
+    getBlogs()
   }, [])
+
+  const handleLogin = async(event) =>{
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      /*window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )*/
+      //noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      console.log('GOT GOOD USER')
+    } catch (exception) {
+      console.log('Wrong credentials')
+    }
+  }
+
+
+  const blogList = () => {
+    if (Array.isArray(blogs))
+      return blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  )
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      { user === null ?
+        loginForm() :
+        blogList()
+      }
     </div>
   )
 }
