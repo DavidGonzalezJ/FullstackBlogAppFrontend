262a3063719d43450752084037935eff5e6f92ee
David <davidgonzalezj97@gmail.com>
2024-01-09 17:54:41 +0100

    Exercise 5.7 Changing the blog component to be able to display more info

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [fullView, setfullView] = useState(false)
+
+  const hideInFullView = { display: fullView ? 'none' : ''}
+  const showInFullView = { display: fullView ? '' : 'none'}
+
+  const toggleFullView = () => {
+    setfullView(!fullView)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+        <button style={hideInFullView} onClick={toggleFullView}>view</button>
+        <button style={showInFullView} onClick={toggleFullView}>hide</button>
+        <div style={showInFullView}>
+          {blog.url}<br />
+          likes {blog.likes} <button style={showInFullView}>like</button> <br />
+          {blog.user && blog.user.name}
+        </div>
+    </div>  
+  )
+}
 
 export default Blog
