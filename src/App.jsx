5f609f105e9f8c5aa652dfcb66f6c343b03a6244
David <davidgonzalezj97@gmail.com>
2024-01-09 18:46:18 +0100

    Exercise 5.10 Showing the blogs ordered by the number of likes

--- src/App.jsx
+++ src/App.jsx
@@ -92,6 +92,7 @@ const App = () => {
   }
 
   const blogList = () => {
+      blogs.sort((a, b) => b.likes - a.likes)
       return blogs.map(blog =>
         <Blog key={blog.id} blog={blog} likeHandler={handleLike}/>)
   }
